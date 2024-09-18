f16796a19b73d4b2aa822aaedf5a56e85653390d
pengfei.lv <pengfei.lv@ucloud.cn>
2024-09-18 20:12:27 +0800

    feat: change api

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -26,20 +26,20 @@
         DB_DATABASE: "",
         BUCKET_NAME: "interview-1256907486",
         BUCKET_REGION: "ap-shanghai",
-        SECRET_ID: "AKIDujS7tafTdbOzQjwSAD9uVwOhokgYdk5J",
+        SECRET_ID: "",
         SECRET_KEY: "EJCuRtCf1FI9oKe4PdjomvtgGe7Ukrpb"
       },
       env_production: {
         PORT: 9000,
         NODE_ENV: 'production',
-        DB_HOST: "81.68.188.127",
+        DB_HOST: "150.158.119.41",
         DB_PORT: 3306,
         DB_USER: "root",
-        DB_PASSWORD: "pwHW9h1bjnhQIbKK",
+        DB_PASSWORD: "mysql123456",
         DB_DATABASE: "uacl",
         BUCKET_NAME: "interview-1256907486",
         BUCKET_REGION: "ap-shanghai",
-        SECRET_ID: "AKIDujS7tafTdbOzQjwSAD9uVwOhokgYdk5J",
+        SECRET_ID: "",
         SECRET_KEY: "EJCuRtCf1FI9oKe4PdjomvtgGe7Ukrpb"
       }
     }
